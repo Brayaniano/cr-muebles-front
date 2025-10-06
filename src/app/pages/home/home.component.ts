6011d7954e396777c059f5b993ce3ad8c5572ab1
wsw-brayan <brayan@webstudioswest.com>
2025-10-06 12:37:32 -0500

    crear paginas

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {BannerComponent} from '../../components/banner/banner.component';
+import {RecomendadosComponent} from '../../components/home/recomendados/recomendados.component';
+import {CalidadComponent} from '../../components/home/calidad/calidad.component';
+import {MasBuscadoComponent} from '../../components/home/mas-buscado/mas-buscado.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [BannerComponent,RecomendadosComponent,CalidadComponent,MasBuscadoComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
